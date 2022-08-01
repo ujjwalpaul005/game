96e0f0bec4c87ca84c8460deb78e3278b40bda73
ujjwalpaul005 <ujjwalpaul005@gmail.com>
2022-08-01 22:34:18 +0530

    all styling done

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+let navbar = () =>{
+    return `
+    <a href="/index.html" id="home">Game Zone</a>
+        
+    <select id="gamelist">
+        <option value="#">Select Game</option>
+        <option value="#">One</option>
+        <option value="#">Two</option>
+    </select>
+    `
+}
+
+export default navbar;
---
+++ script/one_index.js
@@ -0,0 +1,4 @@
+import navbar from "/components/navbar.js";
+
+
+document.getElementById("nav").innerHTML = navbar();
