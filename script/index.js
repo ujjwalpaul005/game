0c19ee0b977ee4e8a4be9f675c1b8e372782b50a
ujjwalpaul005 <ujjwalpaul005@gmail.com>
2022-09-11 10:18:54 +0530

    link modified

--- script/index.js
+++ script/index.js
@@ -4,3 +4,7 @@ import footer from "../components/footer.js";
 
 document.getElementById("nav").innerHTML = navbar();
 document.getElementById("footer").innerHTML = footer();
+
+document.getElementById("home").addEventListener("click", ()=>{
+    window.location.href = "../index.html";
+});
--- script/one_index.js
+++ script/one_index.js
@@ -5,3 +5,7 @@ import footer from "../components/footer.js";
 document.getElementById("nav").innerHTML = navbar();
 
 document.getElementById("footer").innerHTML = footer();
+
+document.getElementById("home").addEventListener("click", ()=>{
+    window.location.href = "../game/index.html";
+});
