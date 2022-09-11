c6b4b4d47e407fa1b7266dd09e56617ad6b837ab
ujjwalpaul005 <ujjwalpaul005@gmail.com>
2022-09-11 10:25:12 +0530

    link modified

--- script/index.js
+++ script/index.js
@@ -5,6 +5,3 @@ import footer from "../components/footer.js";
 document.getElementById("nav").innerHTML = navbar();
 document.getElementById("footer").innerHTML = footer();
 
-document.getElementById("home").addEventListener("click", ()=>{
-    window.location.href = "../game/index.html";
-});
